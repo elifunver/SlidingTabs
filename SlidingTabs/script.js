a568d778ac10dfb39e760b991daa3446047daaf7
Elif Ünver <114211997+elifunver@users.noreply.github.com>
2023-02-10 00:39:45 +0300

    Add files via upload

---
+++ SlidingTabs/script.js
@@ -0,0 +1,79 @@
+function FirstButton()
+        {
+            var firstButton=document.getElementById('firstButton');
+            var secondButton=document.getElementById('secondButton');
+            var thirdButton=document.getElementById('thirdButton');
+            var fourthButton=document.getElementById('fourthButton');
+            var fifthButton=document.getElementById('fifthButton');
+            var slider=document.getElementById('slider');
+            firstButton.style.color="#0b57ba";
+            secondButton.style.color="black";
+            thirdButton.style.color="black";
+            fourthButton.style.color="black";
+            fifthButton.style.color="black";
+            slider.style.left="17px";   
+        }
+
+        function SecondButton()
+        {
+            var firstButton=document.getElementById('firstButton');
+            var secondButton=document.getElementById('secondButton');
+            var thirdButton=document.getElementById('thirdButton');
+            var fourthButton=document.getElementById('fourthButton');
+            var fifthButton=document.getElementById('fifthButton');
+            var slider=document.getElementById('slider');
+            firstButton.style.color="black";
+            secondButton.style.color="#0b57ba";
+            thirdButton.style.color="black";
+            fourthButton.style.color="black";
+            fifthButton.style.color="black";
+            slider.style.left="143px";
+        }
+
+        function ThirdButton()
+        {
+            var firstButton=document.getElementById('firstButton');
+            var secondButton=document.getElementById('secondButton');
+            var thirdButton=document.getElementById('thirdButton');
+            var fourthButton=document.getElementById('fourthButton');
+            var fifthButton=document.getElementById('fifthButton');
+            var slider=document.getElementById('slider');
+            firstButton.style.color="black";
+            secondButton.style.color="black";
+            thirdButton.style.color="#0b57ba";
+            fourthButton.style.color="black";
+            fifthButton.style.color="black";
+            slider.style.left="268px";
+        }
+
+        function FourthButton()
+        {
+            var firstButton=document.getElementById('firstButton');
+            var secondButton=document.getElementById('secondButton');
+            var thirdButton=document.getElementById('thirdButton');
+            var fourthButton=document.getElementById('fourthButton');
+            var fifthButton=document.getElementById('fifthButton');
+            var slider=document.getElementById('slider');
+            firstButton.style.color="black";
+            secondButton.style.color="black";
+            thirdButton.style.color="black";
+            fourthButton.style.color="#0b57ba";
+            fifthButton.style.color="black";
+            slider.style.left="395px";
+        }
+
+        function FifthButton()
+        {
+            var firstButton=document.getElementById('firstButton');
+            var secondButton=document.getElementById('secondButton');
+            var thirdButton=document.getElementById('thirdButton');
+            var fourthButton=document.getElementById('fourthButton');
+            var fifthButton=document.getElementById('fifthButton');
+            var slider=document.getElementById('slider');
+            firstButton.style.color="black";
+            secondButton.style.color="black";
+            thirdButton.style.color="#black";
+            fourthButton.style.color="black";
+            fifthButton.style.color="#0b57ba";
+            slider.style.left="520px";
+        }
